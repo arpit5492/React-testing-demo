7cea37d0460160b8e24a73331827d55568b0ea38
arpit5492 <arpitmallick99@gmail.com>
2024-04-08 21:59:57 -0700

    Implemented Snapshot testing

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -3,34 +3,6 @@ import Counter from "./Counter";
 import {fireEvent, render, screen, waitFor} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-beforeEach(() => {
-    render(<Counter />);
-});
-
-test("Initial counter value should be 0", () => {
-    const counterEle = screen.getByTestId("cnt");
-    expect(counterEle).toBeInTheDocument;
-});
-
-test("On click of button, the counter should be 1", async () => {
-    const btnEle = screen.getByTestId("btn");
-    const counterEle = screen.getByTestId("cnt");
-    expect(counterEle.textContent).toBe("0");
-    fireEvent.click(btnEle);
-    // await userEvent.click(btnEle);
-    // userEvent.click(btnEle);
-    waitFor(() => {
-        expect(counterEle.textContent).toBe("1");
-    });
-    // expect(counterEle.textContent).toBe("1");
-});
-
-test("Input value should change", () => {
-    const inpEle = screen.getByTestId("inp");
-    fireEvent.change(inpEle, {
-        target: {
-            value: "Arpit"
-        }
-    });
-    expect(inpEle.value).toBe("Arpit");
-});
+test("Snapshot test", () => {
+    expect(render(<Counter />)).toMatchSnapshot();
+})
--- src/Users.test.js
+++ src/Users.test.js
@@ -2,11 +2,6 @@ import React from "react";
 import {fireEvent, render, screen, waitFor} from "@testing-library/react";
 import User from "./User";
 
-beforeEach(() => {
-    render(<User />);
-})
-
-test("All the list items are rendered", async () => {
-    const liEle = await screen.findAllByRole("listitem");
-    expect(liEle).not.toHaveLength(0);
-});     
+test("Snapshot test", () => {
+    expect(render(<User />)).toMatchSnapshot();
+});    

076487b5fa6a4913afa930ebb148ae5a9d591a20
arpit5492 <arpitmallick99@gmail.com>
2024-04-08 19:42:30 -0700

    Added userEvent click event testing

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Counter from "./Counter";
-import {fireEvent, render, screen} from "@testing-library/react";
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 test("Initial counter value should be 0", () => {
     const counter = render(<Counter />);
@@ -13,6 +14,9 @@ test("On click of button, the counter should be 1", () => {
     const btnEle = counter.getByTestId("btn");
     const counterEle = counter.getByTestId("cnt");
     expect(counterEle.textContent).toBe("0");
-    fireEvent.click(btnEle);
-    expect(counterEle.textContent).toBe("1");
+    // fireEvent.click(btnEle);
+    userEvent.click(btnEle);
+    waitFor(() => {
+        expect(counterEle.textContent).toBe("1");
+    });
 });
